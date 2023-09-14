7e489943b1436c3055fdd4ec3c9044235e7978ce
Konstantinos Lambrou-Latreille <konstantinos.lambrou@iohk.io>
2023-09-14 11:40:20 -0400

    Fixed IOGX upgrade

--- nix/cabal-project.nix
+++ nix/cabal-project.nix
@@ -1,7 +1,7 @@
 # This file is part of the IOGX template and is documented at the link below:
 # https://www.github.com/input-output-hk/iogx#33-nixcabal-projectnix
 
-{ inputs, inputs', meta, config, pkgs, l, ... }:
+{ inputs, inputs', meta, config, pkgs, lib, ... }:
 
 let
 

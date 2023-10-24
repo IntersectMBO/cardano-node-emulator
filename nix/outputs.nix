e5caf92b72bfdcefc6def7f01266fc2ef1bc15b8
zeme-wana <15709674+zeme-wana@users.noreply.github.com>
2023-10-24 12:11:36 +0200

    Bump IOGX (#5)

---
+++ nix/outputs.nix
@@ -0,0 +1,12 @@
+{ repoRoot, inputs, pkgs, lib, system }:
+
+let
+
+  project = repoRoot.nix.project;
+
+in
+[
+  (
+    project.flake
+  )
+]
---
+++ nix/project.nix
@@ -0,0 +1,39 @@
+{ repoRoot, inputs, pkgs, lib, system }:
+
+let
+
+  cabalProject = pkgs.haskell-nix.cabalProject' {
+    name = "cardano-node-emulator";
+    src = ../.;
+    compiler-nix-name = lib.mkDefault "ghc928";
+    shell.withHoogle = false;
+    inputMap = {
+      "https://input-output-hk.github.io/cardano-haskell-packages" = inputs.iogx.inputs.CHaP;
+    };
+    sha256map = {
+      "https://github.com/input-output-hk/cardano-node"."a158a679690ed8b003ee06e1216ac8acd5ab823d" = "sha256-uY7wPyCgKuIZcGu0+vGacjGw2kox8H5ZsVGsfTNtU0c=";
+    };
+    modules = [{
+      packages = {
+        # Werror everything. This is a pain, see https://github.com/input-output-hk/haskell.nix/issues/519
+        cardano-node-emulator.ghcOptions = [ "-Werror" ];
+        cardano-node-socket-emulator.ghcOptions = [ "-Werror" ];
+        freer-extras.ghcOptions = [ "-Werror" ];
+        plutus-ledger.ghcOptions = [ "-Werror" ];
+        plutus-script-utils.ghcOptions = [ "-Werror" ];
+      };
+    }];
+  };
+
+  project = lib.iogx.mkHaskellProject {
+    inherit cabalProject;
+    shellArgs = repoRoot.nix.shell;
+    readTheDocs = {
+      enable = true;
+      siteFolder = "doc/read-the-docs-site";
+    };
+  };
+
+in
+
+project
